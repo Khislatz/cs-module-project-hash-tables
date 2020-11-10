bf437629d23903e3dfff8b135a0c904657cf1c67
Khislatz <47505600+Khislatz@users.noreply.github.com>
2020-11-09 20:42:13 -0500

    expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,11 +1,20 @@
-# Your code here
-
+cache = {}
 
 def expensive_seq(x, y, z):
-    # Your code here
+    """
+exps(x, y, z) =
+     if x <= 0: y + z
+     if x >  0: exps(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
+    """
+    arguments = (x, y, z)
+    if x <=0:
+        return y + z
 
-
-
+    if arguments not in cache:
+        cache[arguments] = expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+    
+    return cache[arguments]
+    
 if __name__ == "__main__":
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
